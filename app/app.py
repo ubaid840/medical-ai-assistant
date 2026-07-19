e1d5b204611aa8d79bdc267e1a98f58cf01ebd69
Ubaid Ashraf <ubaidashraf876@gmail.com>
2026-07-19 23:43:33 +0530

    Fix Streamlit Cloud hang with pysqlite3 and disabled telemetry

--- app/app.py
+++ app/app.py
@@ -1,3 +1,10 @@
+import os
+os.environ["ANONYMIZED_TELEMETRY"] = "False"
+
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 from pathlib import Path
 import streamlit as st
 
