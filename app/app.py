975442e1f0a00cb472b5eb0e05352f8e80c4e52e
Ubaid Ashraf <ubaidashraf876@gmail.com>
2026-07-25 21:51:05 +0530

    Fix: Make pysqlite3 import safe for local Windows execution

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
 import os
 os.environ["ANONYMIZED_TELEMETRY"] = "False"
 
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+try:
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
 
 from pathlib import Path
 import streamlit as st
